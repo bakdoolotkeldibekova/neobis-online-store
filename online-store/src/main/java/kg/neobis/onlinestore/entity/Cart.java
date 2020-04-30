d0b3090d47111f247480426e10cdfebbeb2c6e73
bakdoolotkeldibekova <bakdoolotkeldibekova@gmail.com>
2020-04-30 22:24:26 +0600

    cart, order entities

--- online-store/src/main/java/kg/neobis/onlinestore/entity/Cart.java
+++ online-store/src/main/java/kg/neobis/onlinestore/entity/Cart.java
@@ -1,23 +1,21 @@
-//package kg.neobis.onlinestore.entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import javax.persistence.*;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
-//
-//@Entity
-//@Table(name = "cart")
-//public class Cart extends BaseEntity{
-//    @OneToOne
-//    @JoinColumn(name = "customer_id")
-//    private Customer customer;
-//    @OneToMany(mappedBy = "cart")
-//    private List<CartItem> cartItemList = new ArrayList<>();
-//}
+package kg.neobis.onlinestore.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "cart")
+public class Cart extends BaseEntity{
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
